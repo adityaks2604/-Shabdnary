7290022bcd2db256515a425a339a4de69f6ea4e4
Adityaks <krsinghaditya98@gmail.com>
2022-09-11 17:44:37 +0530

    commit 2

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,7 +12,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
     );
   }
 }
